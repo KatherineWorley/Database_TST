52c24d1249adf2ba77e94ee948b37c22df4d3834
Katherine Worley <kdesign216@yahoo.com>
2018-04-08 16:35:16 -0400

    added method to Person class

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,7 @@
 class Person < ApplicationRecord
 	has_many :orders
+
+	def self.search(term, page = 1, per_page = 10)
+    	Person.offset( per_page * (page - 1)).limit(per_page).find_all_by_last_name term
+	end
 end
